f34514aaf1fb818e4e314f657c8475b889a1f606
maycon djeison <s.maycon@aluno.ifsp.edu.br>
2025-04-22 23:35:01 -0300

    Add files via upload

---
+++ Execicio(5).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double valor1;
+	double valor2;
+	double valor3;
+	double valor4;
+	double ma;
+	
+	cout << "informe  o valor1:";
+	cin >> valor1;
+	
+	cout << "informe  o valor2 :";
+	cin >> valor2 ;
+	
+cout << "informe  o valor3:";
+	cin >> valor3 ;
+	
+cout << "informe  o valor4 :";
+	cin >> valor4;
+	
+	ma =(valor1 +valor2 +valor3+valor4)/4;
+	
+	cout << " a media aritmetica eh " << 
+	ma<< endl;
+	
+	return 0;
+}
