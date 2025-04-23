d8de86d63acf54fe6282dae6090be4553462bb5a
maycon djeison <s.maycon@aluno.ifsp.edu.br>
2025-04-22 23:33:59 -0300

    Add files via upload

---
+++ Exercício(2).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int aresta ;
+	int area;
+	
+	cout << "informe a  aresta do quadrado:";
+	cin >> aresta;
+	
+	area = aresta*aresta;
+	
+	cout << " a area do quadrado  eh  "	<< 
+	area << endl;
+	
+	return 0;
+}
