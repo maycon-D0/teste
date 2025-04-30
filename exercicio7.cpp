aa098d21e046c4b2369e9960d3f5609eaadc1a1e
maycon djeison <s.maycon@aluno.ifsp.edu.br>
2025-04-30 20:31:53 -0300

    Add files via upload

---
+++ exercicio7.cpp
@@ -0,0 +1,21 @@
+    #include <iostream>
+   using namespace std;
+
+   int main()
+{
+	double mm;
+	double x=1.852;
+	double mmt;
+	
+	cout << "informe a quantidade de milhas maritimas:";
+	cin >>mm;
+	
+	mmt=mm*x;
+	
+	cout<<"a quantidade de milhas maritimas em metros eh "
+	<<mmt<<endl;
+	
+	return 0;
+	
+	
+}
