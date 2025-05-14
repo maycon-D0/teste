3a88b5840835605648aaa05a815e73505610afc9
maycon djeison <s.maycon@aluno.ifsp.edu.br>
2025-05-14 20:19:38 -0300

    Add files via upload

---
+++ exercicio 11.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+#include <math.h>
+using namespace std;
+ int main()
+{
+	double x;
+	double y;
+	double re;
+	cout <<" informe o valor de x:";
+	cin>>x;
+	cout<<" informe o valor de y:";
+	cin >>y;
+	re=(pow((x),y));
+	cout<<"o resultado eh "<<re<<endl;
+	return 0;
+}
