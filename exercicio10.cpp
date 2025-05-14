2b63ebb94529930b3dcf3f34bbb621525cad83c6
maycon djeison <s.maycon@aluno.ifsp.edu.br>
2025-05-14 20:04:40 -0300

    Add files via upload

---
+++ exercicio10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+   
+    int main()
+ 
+ {
+ 	double cot;
+ 	double dolar;
+ 	double re;
+ 	
+ 	cout<< "informe  a cotacao do dolar:";
+ 	cin>>cot;
+ 	cout<< "informe a quantidade de dolares:";
+ 	cin>>dolar;
+ 	re=cot*dolar;
+ 	
+ 	cout<<"a quantidade de dolar em reais eh   " <<re << endl;
+ 	
+ 	return 0;
+ }
